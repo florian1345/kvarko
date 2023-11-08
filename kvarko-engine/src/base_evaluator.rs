af0e08fd92a87153b1aa319c05f6af956307e68b
florian1345 <florian1345@gmx.de>
2023-11-08 21:29:04 +0100

    add more tests for base evaluator

--- kvarko-engine/src/base_evaluator.rs
+++ kvarko-engine/src/base_evaluator.rs
@@ -211,7 +211,7 @@ pub fn evaluate_if_no_moves_remain(check: bool) -> f32 {
 impl<H: PositionHasher> BaseEvaluator<H> for KvarkoBaseEvaluator {
 
     fn evaluate_state_with_precomputed_data(&mut self, state: &mut State<H>,
-        _: f32, _: f32, move_count: usize, check: bool) -> f32 {
+            _: f32, _: f32, move_count: usize, check: bool) -> f32 {
         if state.is_stateful_draw() {
             return 0.0;
         }
@@ -243,26 +243,198 @@ mod tests {
     use kvarko_model::hash::ZobristHasher;
 
     use kernal::prelude::*;
+    use kvarko_model::movement::Move;
 
     use super::*;
+
+    fn make_move(state: &mut State<ZobristHasher<u64>>, algebraic: &str) {
+        let mov = Move::parse_algebraic(state.position(), algebraic).unwrap();
+
+        state.make_move(&mov);
+    }
+
+    fn eval_with_kvarko_base_evaluator(state: &mut State<ZobristHasher<u64>>)
+            -> f32 {
+        let mut base_evaluator = KvarkoBaseEvaluator::default();
+
+        base_evaluator.evaluate_state(state, f32::NEG_INFINITY, f32::INFINITY)
+    }
 
     #[test]
     fn base_evaluator_punishes_doubled_pawns() {
         // In both scenarios, players have equal number of available moves.
 
-        let no_doubled_pawns_fen = "8/4k3/4p3/3pP3/3P4/3K4/8/8 w - - 0 1";
-        let mut no_doubled_pawns: State<ZobristHasher<u64>> =
-            State::from_fen(no_doubled_pawns_fen).unwrap();
-        let doubled_pawns_fen = "8/8/3Ppk2/3p4/3P4/3K4/8/8 w - - 0 1";
-        let mut doubled_pawns: State<ZobristHasher<u64>> =
-            State::from_fen(doubled_pawns_fen).unwrap();
-        let mut base_evaluator = KvarkoBaseEvaluator::default();
+        let mut no_doubled_pawns =
+            State::from_fen("8/4k3/4p3/3pP3/3P4/3K4/8/8 w - - 0 1").unwrap();
+        let mut doubled_pawns =
+            State::from_fen("8/8/3Ppk2/3p4/3P4/3K4/8/8 w - - 0 1").unwrap();
 
-        let no_doubled_pawns_eval = base_evaluator.evaluate_state(
-            &mut no_doubled_pawns, f32::NEG_INFINITY, f32::INFINITY);
-        let doubled_pawns_eval = base_evaluator.evaluate_state(
-            &mut doubled_pawns, f32::NEG_INFINITY, f32::INFINITY);
+        let no_doubled_pawns_eval =
+            eval_with_kvarko_base_evaluator(&mut no_doubled_pawns);
+        let doubled_pawns_eval =
+            eval_with_kvarko_base_evaluator(&mut doubled_pawns);
 
         assert_that!(no_doubled_pawns_eval).is_greater_than(doubled_pawns_eval + 0.01);
+    }
+
+    #[test]
+    fn base_evaluator_has_evaluation_zero_for_draw_by_repetition() {
+        let mut state =
+            State::from_fen("5K1k/7q/8/8/8/8/R7/r7 w - - 0 1").unwrap();
+
+        for _ in 0..2 {
+            make_move(&mut state, "Rb2");
+            make_move(&mut state, "Rb1");
+            make_move(&mut state, "Ra2");
+            make_move(&mut state, "Ra1");
+        }
+
+        let eval = eval_with_kvarko_base_evaluator(&mut state);
+
+        assert_that!(eval).is_zero();
+    }
+
+    #[test]
+    fn base_evaluator_has_evaluation_zero_for_draw_by_fifty_move_rule() {
+        let mut state =
+            State::from_fen("5K1k/7q/8/8/8/8/R7/r7 b - - 100 60").unwrap();
+
+        let eval = eval_with_kvarko_base_evaluator(&mut state);
+
+        assert_that!(eval).is_zero();
+    }
+
+    #[test]
+    fn base_evaluator_has_evaluation_zero_for_draw_by_insufficient_material() {
+        let mut state =
+            State::from_fen("5K1k/7b/8/8/8/8/8/8 w - - 0 1").unwrap();
+
+        let eval = eval_with_kvarko_base_evaluator(&mut state);
+
+        assert_that!(eval).is_zero();
+    }
+
+    #[test]
+    fn base_evaluator_has_evaluation_zero_for_stalemate() {
+        // Board (black to move):
+        // ┌───┬───┬───┬───┬───┬───┬───┬───┐
+        // │ k │   │   │   │   │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │   │ Q │ K │   │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │   │   │   │   │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │   │   │   │   │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │   │   │   │   │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │   │   │   │   │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │   │   │   │   │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │   │   │   │   │   │   │   │
+        // └───┴───┴───┴───┴───┴───┴───┴───┘
+        //
+        // Black has no legal moves.
+
+        let mut state =
+            State::from_fen("k7/2QK4/8/8/8/8/8/8 b - - 0 1").unwrap();
+
+        let eval = eval_with_kvarko_base_evaluator(&mut state);
+
+        assert_that!(eval).is_zero();
+    }
+
+    #[test]
+    fn base_evaluator_has_correct_evaluation_for_checkmate() {
+        // Board (white to move):
+        // ┌───┬───┬───┬───┬───┬───┬───┬───┐
+        // │   │   │   │   │   │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │   │   │   │   │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │   │   │   │   │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │   │   │   │   │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │   │   │   │   │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │ k │   │   │   │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │   │ q │   │   │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │   │ K │   │   │   │   │   │
+        // └───┴───┴───┴───┴───┴───┴───┴───┘
+        //
+        // Black checkmated White.
+
+        let mut state =
+            State::from_fen("8/8/8/8/8/1k6/2q5/2K5 w - - 0 1").unwrap();
+
+        let eval = eval_with_kvarko_base_evaluator(&mut state);
+
+        assert_that!(eval).is_equal_to(-CHECKMATE_VALUE);
+    }
+
+    #[test]
+    fn base_evaluator_favors_clear_material_advantage() {
+        // Board (black to move):
+        // ┌───┬───┬───┬───┬───┬───┬───┬───┐
+        // │   │   │   │   │   │   │   │ k │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │   │   │   │   │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │   │   │   │   │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │   │   │   │   │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │   │   │   │ b │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │   │   │   │   │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │ K │   │   │   │   │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │ R │   │   │   │   │   │   │   │
+        // └───┴───┴───┴───┴───┴───┴───┴───┘
+        //
+        // Black has more legal moves, but White is up an exchange.
+
+        let mut state =
+            State::from_fen("7k/8/8/8/4b3/8/K7/R7 b - - 0 1").unwrap();
+
+        let eval = eval_with_kvarko_base_evaluator(&mut state);
+
+        assert_that!(eval).is_negative();
+    }
+
+    #[test]
+    fn base_evaluator_favors_mobility() {
+        // Board (white to move):
+        // ┌───┬───┬───┬───┬───┬───┬───┬───┐
+        // │   │   │   │   │   │ b │   │ k │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │   │   │   │   │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │   │   │   │   │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │   │   │   │   │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │   │   │   │ B │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │   │   │   │   │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │ K │   │   │   │   │   │   │
+        // ├───┼───┼───┼───┼───┼───┼───┼───┤
+        // │   │   │   │   │   │   │   │   │
+        // └───┴───┴───┴───┴───┴───┴───┴───┘
+        //
+        // Although material is equal, White has more available moves.
+
+        let mut state =
+            State::from_fen("5b1k/8/8/8/4B3/8/1K6/8 w - - 0 1").unwrap();
+
+        let eval = eval_with_kvarko_base_evaluator(&mut state);
+
+        assert_that!(eval).is_positive();
     }
 }
