21b2e0e7fab0c5602a23af73fba5583db73fa810
florian1345 <florian1345@gmx.de>
2024-04-26 20:09:16 +0200

    document evaluation type

--- kvarko-engine/src/eval.rs
+++ kvarko-engine/src/eval.rs
@@ -5,10 +5,23 @@ use serde::{Deserialize, Serialize};
 
 pub type Centipawns = i16;
 
-const MAX_CENTIPAWNS: Centipawns = 30_000;
-const MIN_CENTIPAWNS: Centipawns = -MAX_CENTIPAWNS;
+/// The maximum centipawn evaluation representable by [Evaluation].
+pub const MAX_CENTIPAWNS: Centipawns = 30_000;
+
+/// The minimum centipawn evaluation representable by [Evaluation].
+pub const MIN_CENTIPAWNS: Centipawns = -MAX_CENTIPAWNS;
+
 const CHECKMATED_VALUE: Centipawns = Centipawns::MAX - 1;
 
+/// The evaluation of a position, indicating how good it is for the active player. This comes in two
+/// different flavors.
+///
+/// * **Centipawn-evaluations** represent situations in which no forced-checkmate-sequence could be
+/// found and contain an evaluation measured in centipawns (1/100 of a pawn's material value). They
+/// are compared by their centipawn value.
+/// * **Forced-checkmate-evaluations** represent situations in which a forced-checkmate-sequence was
+/// found. They are distinguished by the length of the sequence, with a shorter sequence being
+/// considered superior for the player who is winning.
 #[derive(Clone, Copy, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 pub struct Evaluation(Centipawns);
 
@@ -20,14 +33,46 @@ impl Evaluation {
     /// A theoretical evaluation better than checkmate in 1 for the active player.
     pub const INFINITY: Evaluation = Evaluation(Centipawns::MAX);
 
+    /// An evaluation of zero, indicating that neither player has an advantage. This is also the
+    /// evaluation of a draw.
     pub const ZERO: Evaluation = Evaluation(0);
+    
+    /// An evaluation from the perspective of the player who has been checkmated, i.e. the worst
+    /// possible achievable evaluation.
     pub const CHECKMATED: Evaluation = Evaluation(-CHECKMATED_VALUE);
 
+    /// Constructs a centipawn-evaluation (i.e. no forced checkmate) from the centipawn value,
+    /// _without_ checking whether the value is in the legal range for centipawn evaluation. This
+    /// condition is violated for too high or too low centipawn evaluations, which reach the range
+    /// used by the [Evaluation] type internally to represent forced-checkmate-evaluations. In this
+    /// case, the resulting evaluation will be an undefined forced-checkmate-evaluation. If you
+    /// are not sufficiently sure that the given centipawns are in the legal range, use
+    /// [Evaluation::from_centipawns] instead.
+    ///
+    /// # Arguments
+    ///
+    /// * `centipawns`: The centipawn value of the evaluation to construct. Must be in the range
+    /// [MIN_CENTIPAWNS] to [MAX_CENTIPAWNS], otherwise the result is not well-defined.
+    ///
+    /// # Returns
+    ///
+    /// An evaluation representing the given amount of centipawns.
     #[inline]
     pub fn from_centipawns_unchecked(centipawns: Centipawns) -> Evaluation {
         Evaluation(centipawns)
     }
 
+    /// Constructs a centipawn-evaluation (i.e. no forced checkmate) from the centipawn value.
+    ///
+    /// # Arguments
+    ///
+    /// * `centipawns`: The centipawn value of the evaluation to construct. Must be in the range
+    /// [MIN_CENTIPAWNS] to [MAX_CENTIPAWNS].
+    ///
+    /// # Returns
+    ///
+    /// An evaluation representing the given amount of centipawns or `None`, if `centipawns` is
+    /// outside the legal range.
     #[inline]
     pub fn from_centipawns(centipawns: Centipawns) -> Option<Evaluation> {
         let evaluation = Evaluation(centipawns);
@@ -40,6 +85,14 @@ impl Evaluation {
         }
     }
 
+    /// Computes the evaluation viewed from the opponent's perspective one ply before the state
+    /// evaluated by this evaluation. This is the inverse of the current evaluation, with
+    /// potentially a one ply longer forced checkmate sequence in case of a
+    /// forced-checkmate-evaluation.
+    ///
+    /// # Returns
+    ///
+    /// The evaluation viewed from the opponent's perspective one ply earlier.
     #[inline]
     pub fn in_previous_turn(self) -> Evaluation {
         let mut evaluation = Evaluation(-self.0);
@@ -51,21 +104,48 @@ impl Evaluation {
         evaluation
     }
 
+    /// Indicates whether this evaluation represents a forced-checkmate-evaluation for either the
+    /// active player or their opponent.
+    ///
+    /// # Returns
+    ///
+    /// `true` if this evaluation is a forced-checkmate-evaluation and `false` if it is a
+    /// centipawn-evaluation.
     #[inline]
     pub fn is_forced_checkmate(self) -> bool {
         self.is_forced_checkmate_for_turn() || self.is_forced_checkmate_for_opponent()
     }
 
+    /// Indicates whether this evaluation represents a forced-checkmate-evaluation for the active
+    /// player.
+    ///
+    /// # Returns
+    ///
+    /// `true` if this evaluation is a forced-checkmate-evaluation for the active player and `false`
+    /// if it is a centipawn-evaluation or a forced-checkmate-evaluation for the opponent.
     #[inline]
     pub fn is_forced_checkmate_for_turn(self) -> bool {
         self.0 > MAX_CENTIPAWNS
     }
 
+    /// Indicates whether this evaluation represents a forced-checkmate-evaluation for the opponent
+    /// of the active player.
+    ///
+    /// # Returns
+    ///
+    /// `true` if this evaluation is a forced-checkmate-evaluation for the opponent of the active
+    /// player and `false` if it is a centipawn-evaluation or a forced-checkmate-evaluation for the
+    /// active player.
     #[inline]
     pub fn is_forced_checkmate_for_opponent(self) -> bool {
         self.0 < MIN_CENTIPAWNS
     }
 
+    /// Gets the centipawn-value of this evaluation.
+    ///
+    /// # Returns
+    ///
+    /// The centipawn-value of this evaluation or `None`, if it is a forced-checkmate-evaluation.
     pub fn as_centipawns(self) -> Option<Centipawns> {
         if self.is_forced_checkmate() {
             None
@@ -75,6 +155,15 @@ impl Evaluation {
         }
     }
 
+    /// Gets the length of the forced-checkmate-sequence represented by a
+    /// forced-checkmate-evaluation. A positive number represents a forced-checkmate-sequence for
+    /// the active player, a negative number one for the opponent. The magnitude represents the
+    /// number of moves.
+    ///
+    /// # Returns
+    ///
+    /// The length of the forced-checkmate-sequence or `None`, if this evaluation is a
+    /// centipawn-evaluation.
     pub fn as_checkmate_in(self) -> Option<Centipawns> {
         if self.is_forced_checkmate() {
             Some((CHECKMATED_VALUE - self.0.abs()) * self.0.signum())
@@ -84,11 +173,29 @@ impl Evaluation {
         }
     }
 
+    /// Returns the smaller (i.e. worse for the active player) evaluation of two.
+    ///
+    /// # Arguments
+    ///
+    /// `other`: The other evaluation to compare to this one.
+    ///
+    /// # Return
+    ///
+    /// The smaller evaluation between this one and the given `other`.
     #[inline]
     pub fn min(self, other: Evaluation) -> Evaluation {
         Evaluation(self.0.min(other.0))
     }
 
+    /// Returns the larger (i.e. better for the active player) evaluation of two.
+    ///
+    /// # Arguments
+    ///
+    /// `other`: The other evaluation to compare to this one.
+    ///
+    /// # Return
+    ///
+    /// The larger evaluation between this one and the given `other`.
     #[inline]
     pub fn max(self, other: Evaluation) -> Evaluation {
         Evaluation(self.0.max(other.0))
